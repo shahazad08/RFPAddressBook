6c60ea411fa235bf3f883d602a6f351d892ce11a
unknown <shaikh.shahazad@bridgelabz.com>
2024-02-08 16:47:08 +0530

    Added: Contact Details Edited

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -3,6 +3,8 @@ package com.bridgelabz;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class AddressBook {
     ArrayList<Contact> contacts = new ArrayList<Contact>();
     Contact contact=new Contact();
@@ -44,7 +46,95 @@ public class AddressBook {
 
         contacts.add(contact);
 
-        System.out.println("Contact List"+contacts);
 
     }
+
+    public void editContact() {
+        System.out.println("Enter the name you want to edit");
+        char continueChoice;
+        String firstName=sc.nextLine();
+        if(firstName.equals(contact.getFirstName())) {
+           do {
+                System.out.println("What do you want to edit");
+                System.out.println("1. First Name");
+                System.out.println("2. Last Name");
+                System.out.println("3. Address");
+                System.out.println("4. City");
+                System.out.println("5. State");
+                System.out.println("6. Zip Code");
+                System.out.println("7. Email");
+                System.out.println("8. Mobil Number");
+                int ch = sc.nextInt();
+                switch (ch) {
+                    case 1:
+                        System.out.println("Enter the new name you want to edit");
+                        String newName = sc.next();
+                        contact.setFirstName(newName);
+
+                        break;
+                    case 2:
+                        System.out.println("Enter the new Last Name  you want to edit");
+                        String newLastName = sc.next();
+                        contact.setLastName(newLastName);
+                        break;
+                    case 3:
+                        System.out.println("Enter the new Address you want to edit");
+                        String newAddress = sc.next();
+                        contact.setAddress(newAddress);
+                        break;
+                    case 4:
+                        System.out.println("Enter the new City you want to edit");
+                        String newCity = sc.next();
+                        contact.setCity(newCity);
+                        break;
+                    case 5:
+                        System.out.println("Enter the new State you want to edit");
+                        String newState = sc.next();
+                        contact.setState(newState);
+                        break;
+                    case 6:
+                        System.out.println("Enter the new Zip Code you want to edit");
+                        int newZip = sc.nextInt();
+                        contact.setZip(newZip);
+                        break;
+                    case 7:
+                        System.out.println("Enter the new Email you want to edit");
+                        String newEmail = sc.next();
+                        contact.setEmailId(newEmail);
+                        break;
+                    case 8:
+                        System.out.println("Enter the new Mobile Number  you want to edit");
+                        long newMobileNumber = sc.nextLong();
+                        contact.setPhoneNumber(newMobileNumber);
+                        break;
+                    case 9:
+
+                        System.out.println("Exiting the program. Goodbye!");
+                        sc.close(); // Close the scanner before exiting
+                        System.exit(0);
+                    default:
+                        System.out.println("Please enter the valid Choice");
+                        exit(0);
+
+                }
+               System.out.print("Do you want to continue? (y/n): ");
+               sc.nextLine(); // Consume the newline character left by nextInt()
+               continueChoice = sc.nextLine().charAt(0);
+           } while (continueChoice == 'y' || continueChoice == 'Y');
+
+            System.out.println("Program terminated. Goodbye!");
+            }
+        else {
+            System.out.println("Name is not found");
+        }
+
+        }
+
+
+    public void displayContact() {
+        System.out.println("contact list are:");
+        for(Contact contact:contacts)
+            System.out.println(contact);
+    }
 }
+
--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,10 +1,40 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to address Book");
+        Scanner sc=new Scanner(System.in);
         AddressBook addressBook=new AddressBook();
-        addressBook.createContact();
+        while (true) {
+            System.out.println("Address Book Menu:");
+            System.out.println("1. Create Contact");
+            System.out.println("2. Display Contacts");
+            System.out.println("3. Edit Contact");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    addressBook.createContact();
+                    break;
+                case 2:
+                    addressBook.displayContact();
+                    break;
+                case 3:
+                    addressBook.editContact();
+                    break;
+                case 4:
+                    System.out.println("Exiting the program. Goodbye!");
+                    sc.close(); // Close the scanner before exiting
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
 
     }
-}
