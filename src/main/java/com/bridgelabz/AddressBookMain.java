3156eb2a010b008cf30789f721dff46e30aa1b04
unknown <shaikh.shahazad@bridgelabz.com>
2024-02-15 12:28:28 +0530

    Added: Implemented using Interface approach

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -9,7 +9,7 @@ import static java.lang.System.exit;
 
 public class AddressBookMain {
     private static Map<String, AddressBook> addressBooks = new HashMap<>();
-    static AddressBook addressBook = new AddressBook();
+   // static AddressBook addressBook = new AddressBook();
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -60,23 +60,7 @@ public class AddressBookMain {
 
         if (addressBooks.containsKey(addressBookName)) {
             AddressBook addressBook = addressBooks.get(addressBookName);
-
-            System.out.println("Enter the first name of the contact you want to delete:");
-            String targetFirstName = sc.nextLine();
-
-            Iterator<Contact> iterator = addressBook.contacts.iterator();
-
-            while (iterator.hasNext()) {
-                Contact contact = iterator.next();
-
-                if (contact.getFirstName().equalsIgnoreCase(targetFirstName)) {
-                    iterator.remove(); // Remove the matching contact
-                    System.out.println("Contact deleted successfully.");
-                    return;
-                }
-            }
-
-            System.out.println("Contact with name " + targetFirstName + " not found.");
+            addressBook.deleteContact();
         } else {
             System.out.println("Address Book '" + addressBookName + "' does not exist.");
         }
@@ -89,93 +73,14 @@ public class AddressBookMain {
         String addressBookName = sc.nextLine();
         if (addressBooks.containsKey(addressBookName)) {
             AddressBook addressBook = addressBooks.get(addressBookName);
-            System.out.println("Enter the first name of the contact you want to edit:");
-            String targetFirstName = sc.nextLine();
-            boolean contactFound = false;
-            char continueChoice;
-            Iterator<Contact> iterator = addressBook.contacts.iterator();
-
-            while (iterator.hasNext()) {
-                Contact contact = iterator.next();
-
-                if (contact.getFirstName().equalsIgnoreCase(targetFirstName)) {
-                    do {
-                        System.out.println("What do you want to edit");
-                        System.out.println("1. First Name");
-                        System.out.println("2. Last Name");
-                        System.out.println("3. Address");
-                        System.out.println("4. City");
-                        System.out.println("5. State");
-                        System.out.println("6. Zip Code");
-                        System.out.println("7. Email");
-                        System.out.println("8. Mobil Number");
-                        int ch = sc.nextInt();
-                        switch (ch) {
-                            case 1:
-                                System.out.println("Enter the new name you want to edit");
-                                String newName = sc.next();
-                                contact.setFirstName(newName);
-
-                                break;
-                            case 2:
-                                System.out.println("Enter the new Last Name  you want to edit");
-                                String newLastName = sc.next();
-                                contact.setLastName(newLastName);
-                                break;
-                            case 3:
-                                System.out.println("Enter the new Address you want to edit");
-                                String newAddress = sc.next();
-                                contact.setAddress(newAddress);
-                                break;
-                            case 4:
-                                System.out.println("Enter the new City you want to edit");
-                                String newCity = sc.next();
-                                contact.setCity(newCity);
-                                break;
-                            case 5:
-                                System.out.println("Enter the new State you want to edit");
-                                String newState = sc.next();
-                                contact.setState(newState);
-                                break;
-                            case 6:
-                                System.out.println("Enter the new Zip Code you want to edit");
-                                int newZip = sc.nextInt();
-                                contact.setZip(newZip);
-                                break;
-                            case 7:
-                                System.out.println("Enter the new Email you want to edit");
-                                String newEmail = sc.next();
-                                contact.setEmailId(newEmail);
-                                break;
-                            case 8:
-                                System.out.println("Enter the new Mobile Number  you want to edit");
-                                long newMobileNumber = sc.nextLong();
-                                contact.setPhoneNumber(newMobileNumber);
-                                break;
-
-                            default:
-                                System.out.println("Please enter the valid Choice");
-                                exit(0);
-
-                        }
-                        System.out.print("Do you want to continue? (y/n): ");
-                        sc.nextLine(); // Consume the newline character left by nextInt()
-                        continueChoice = sc.nextLine().charAt(0);
-                    } while (continueChoice == 'y' || continueChoice == 'Y');
-                    addressBook.editContact();
-
-//                    System.out.println("Program terminated. Goodbye!");
-                    contactFound=true;
-                    break;
-                }
-            }
-
-            if (!contactFound) {
-                System.out.println("Contact with name " + targetFirstName + " not found.");
-            }
-        } else {
+            addressBook.editContact();
+        }
+        else {
             System.out.println("Address Book '" + addressBookName + "' does not exist.");
         }
+
+
+
     }
 
 
---
+++ src/main/java/com/bridgelabz/AddressBookOperations.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public interface AddressBookOperations {
+    void addContact(Contact contact);
+    void displayContacts();
+    void editContact();
+    void deleteContact();
+}
